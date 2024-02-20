238719461d336a4ac35b51f753d22c415e5ad528
sdhan <hsd970730@naver.com>
2024-02-20 22:46:40 +0900

    [week5-LEETCODE] 217. valid anagram
    [week5-LEETCODE] 217. valid anagram

---
+++ leetcode/han/test/valid_anagram_test.py
@@ -0,0 +1,13 @@
+from valid_anagram import Solution
+
+s = "aa"
+t = "a"
+print(Solution().isAnagram(s, t))
+
+s = "anagram"
+t = "nagaram"
+print(Solution().isAnagram(s, t))
+
+s = "rat"
+t = "car"
+print(Solution().isAnagram(s, t))
