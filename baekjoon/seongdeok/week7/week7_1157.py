06fa25b84233d97567993189dc5e83044f6bd5b2
sdhan <hsd970730@naver.com>
2024-03-07 23:56:28 +0900

    week8 - Readme Update
    Readme Update

--- baekjoon/seongdeok/week7/week7_1157.py
+++ baekjoon/seongdeok/week7/week7_1157.py
@@ -2,22 +2,14 @@ str = input()
 
 map = {}
 for s in str:
-    #casefold()
     s = s.upper()
     map[s] = map[s] + 1\
             if s in map\
             else 1
-# print(map)
 
 max_value = max(map.values())
-#print(max_value)
 
-max_keys=[
-    k 
-    for k, v in map.items() 
-    if v == max_value
-]
-#print(max_keys)
+max_keys=[k for k, v in map.items() if v == max_value]
 
 print("?")\
     if len(max_keys) > 1\
