7b96c744008eca9ba002a3c71a8ddc4b36c32a45
sdhan <hsd970730@naver.com>
2024-03-21 23:47:23 +0900

    [BAEKJOON] Z
    1074 Z

---
+++ baekjoon/seongdeok/1074-Z.py
@@ -0,0 +1,28 @@
+def z(n: int, r: int, c:int ) -> int:
+    if n == 0: return 0
+    mid = pow(2, n-1)
+    
+    if r < mid:
+        if c < mid:
+            return z(n-1, r, c)
+        else: 
+            return z(n-1, r, c - mid) + pow(mid, 2)
+    else:
+        if c < mid:
+            return z(n-1, r - mid, c) + 2 * pow(mid, 2)
+        else:
+            return z(n-1, r - mid, c - mid) + 3 * pow(mid, 2)
+    
+n, r, c = map(int, input().split())
+print(z(n, r, c))
+
+
+
+
+
+
+
+
+
+
+
