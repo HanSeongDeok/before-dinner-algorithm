347a329698aa3045c5caeb505a947134852d8791
Chaeeun <78066837+rachaen@users.noreply.github.com>
2024-05-15 21:04:49 +0900

    Delete week12_147355.py

--- week12_147355.py
+++
@@ -1,7 +0,0 @@
-def solution(t, p):
-    answer = 0
-    for i in range(len(t) - len(p) + 1):
-        sliced = t[i : i + len(p)]
-        if int(sliced) <= int(p):
-            answer += 1
-    return answer
