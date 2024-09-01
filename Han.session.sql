0e80847f46b7684959f0fa3559ca8282cd1f67ad
sdhan <hsd970730@naver.com>
2024-09-01 20:50:12 +0900

    [PROGRAMMERS-LEVEL2] 7. 다음 큰 숫자 other way 8. 피보나치 수
    [PROGRAMMERS-LEVEL2] 7. 다음 큰 숫자 other way 8. 피보나치 수
    + MySQL VSC 연동 및 MYSQL 2단계 풀이

---
+++ Han.session.sql
@@ -0,0 +1,17 @@
+-- CREATE TABLE ANIMAL_INS(
+--     ANIMAL_ID VARCHAR(20) NOT NULL,
+--     ANIMAL_TYPE	VARCHAR(20) NOT NULL,
+--     DATETIME DATETIME DEFAULT CURRENT_TIMESTAMP,
+--     INTAKE_CONDITION VARCHAR(20) NOT NULL,
+--     NAME VARCHAR(20),
+--     SEX_UPON_INTAKE	VARCHAR(20)	NOT NULL,
+--     PRIMARY KEY(ANIMAL_ID)
+-- )
+
+INSERT INTO ANIMAL_INS VALUES 
+    ('A396810', 'Dog', '2016-08-22 16:13:00', 'Injured'	,'Raven', 'Spayed Female'), 
+    ('A377750', 'Dog', '2017-10-25 17:17:00', 'Normal', 'Lucy', 'Spayed Female'),
+    ('A355688', 'Dog', '2014-01-26 13:48:00', 'Normal', 'Shadow', 'Neutered Male'),
+    ('A399421', 'Dog', '2014-01-26 13:48:00', 'Normal', 'Lucy', 'Spayed Female'),
+    ('A400680', 'Dog', '2017-06-17 13:29:00', 'Normal', 'Lucy', 'Spayed Female'),
+    ('A410668', 'Dog', '2015-11-19 13:41:00', 'Normal', 'Raven', 'Spayed Female')
