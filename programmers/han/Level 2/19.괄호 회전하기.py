c028809257f0c87eb2ae04d46a072e17d71e1b20
sdhan <hsd970730@naver.com>
2024-10-20 18:22:11 +0900

    [PROGRAMMERS-LEVEL2] 19. 괄호 회전 복습, 20. 할인 행사
    [PROGRAMMERS-LEVEL2] 19. 괄호 회전 복습, 20. 할인 행사

--- programmers/han/Level 2/19.괄호 회전하기.py
+++ programmers/han/Level 2/19.괄호 회전하기.py
@@ -1,15 +1,6 @@
 ## 내일 다시 풀어보기!!
 def solution(s):
     answer = 0
-    open_number_map = {
-        "(" : 1,
-        "[" : 2,
-        "{" : 3,
-        ")" : -1,
-        "]" : -2,
-        "}" : -3,
-    }
-    
     open_close_map = {
         "(" : ")",
         "[" : "]",
@@ -19,14 +10,20 @@ def solution(s):
     size = len(s)
     s *= 2
     for index in range(size):
-        cnt = 0
+        check = True
+        close_list = []
         for v in s[index : index + size]:
-            
-            cnt += open_close_map[v]
-            if cnt < 0:
+            if not close_list and v not in open_close_map:
+                check = False
                 break
-            
-        answer = answer + 1 if cnt == 0 else answer 
+            if v in open_close_map:
+                close_list.append(open_close_map[v])
+            else:
+                if not close_list or v != close_list.pop():
+                    check = False
+                    break
+        if check and not close_list:
+            answer += 1
     return answer
 
 print(solution("[)(]"))
