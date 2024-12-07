a39ec0fe81d3da9c58e3afb54053bfbad3d09ce7
sdhan <hsd970730@naver.com>
2024-12-07 20:46:33 +0900

    [PROGRAMMERS-LEVEL2] 31. 뉴스 클러스터링.py
    [PROGRAMMERS-LEVEL2] 31. 뉴스 클러스터링.py
    - 오답 -> 내일 다시 풀어보기

--- programmers/han/Level 2/31. 뉴스 클러스터링.py
+++ programmers/han/Level 2/31. 뉴스 클러스터링.py
@@ -1,14 +1,31 @@
 def solution(str1, str2):
     answer = 0
-    cnt = 0
-    jacard_1, jacard_2 = [], []
-    chr_1, chr_2 = '', '' 
-    for i, j in str1, str2:
-       chr_1 += i
-       chr_2 += j
-       cnt += 1
-       if cnt == 2:
-           jacard_1.append(i)
-           jacard_2.append(j)
-           cnt = 0 
-    return answer
+    rs_key_upper = lambda str: remove_special_key_upper(str)
+    jacard_1 = create_jacard_arr(rs_key_upper(str1), [])
+    jacard_2 = create_jacard_arr(rs_key_upper(str2), [])
+    intersection = 0
+    A, B = len(jacard_1), len(jacard_2) 
+    for i in jacard_1:
+        if i in jacard_2:
+           intersection += 1
+           jacard_2.remove(i) 
+    union = A + B - intersection   
+    return int((intersection / union) * 65536)
+
+def create_jacard_arr(str, arr):
+    for i in range(len(str)-1):
+       arr.append(str[i:i+2])
+    return arr  
+
+def remove_special_key_upper(str):
+    remove_chr = " ~@#$%!&*()_+-=[]{}|;:'\",.<>?/1234567890"
+    for i in remove_chr:
+        str = str.replace(i,"")
+    return str.upper()    
+    
+    
+
+#print(solution('FRANCE','french'))
+#print(solution('handshake','shake hands'))
+print(solution('aa1+aa2', 'AAAA12'))
+print(solution('E=M*C^2', 'e=m*c^2'))
