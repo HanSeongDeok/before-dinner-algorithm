c4af1a729498d22582c6ecdd42771d507b2eb88e
sdhan <hsd970730@naver.com>
2025-01-20 22:50:26 +0900

    [PROGRAMMERS-LEVEL2] 37. K진수에 소수 개수 구하기
    - 너무 좋은 문제! -
    1. 소수 판별 문제
    2. 진수 변환 문제
    3. 리터럴 활용 문제

--- programmers/han/Level 2/37. K진수에서 소수 개수 구하기.py
+++ programmers/han/Level 2/37. K진수에서 소수 개수 구하기.py
@@ -1,12 +1,28 @@
+import math
+## *** 너무 좋은 문제 *** 
+## 진짜 너무 좋은 문제!!! -> 계속 복습하기
 def solution(n, k):
-    answer = -1
-    return int(conver_k_number(10, 3))
+    conver_k = conver_k_number(n, k)
+    numbers = conver_k.split("0")
+    return sum([1 for i in numbers if i and is_prime(int(i))])
 
-def conver_k_number(n, k):
+def conver_k_number(n: int, k: int) -> str:
     r = ''
     while(k <= n):
-        n //= k
         r += str(n % k)
-    return str(n) + r
+        n //= k
+    return str(n) + r[::-1]
 
-print(solution(10, 3))
+def is_prime(n: int) -> bool:
+    if n <= 1: 
+        return False
+    for i in range(2, int(math.sqrt(n)) + 1):
+        if n % i == 0:
+            return False 
+    return True
+
+# 소수 찾기 -> 아리스토텔레스의 체
+# TODO 복습 하기
+
+print(solution(437674, 3))
+print(solution(110011, 10))
