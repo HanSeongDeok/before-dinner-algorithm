37bc64da2794c83490d4683b243c9b9420f24e78
sdhan <hsd970730@naver.com>
2025-02-17 20:04:03 +0900

    [BAEKJOON] 새싹, 아스키 코드
    - ord, chr
    - print(f"{}")
    - 기본

---
+++ baekjoon/seongdeok/bronze/새싹_25083.py
@@ -0,0 +1,8 @@
+line_1 = "         ,r'\"7"
+line_2 = "r`-_   ,'  ,/"
+line_3 = " \. \". L_r'"
+line_4 = "   `~\/"
+line_5 = "      |"
+line_6 = "      |"
+
+print(f"{line_1}\n{line_2}\n{line_3}\n{line_4}\n{line_5}\n{line_6}")
