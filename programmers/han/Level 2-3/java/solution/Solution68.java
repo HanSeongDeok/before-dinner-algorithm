64ee3afa472070e0ceff63e8f9d609ede3d69b65
sdhan <hsd970730@naver.com>
2025-11-15 17:39:23 +0900

    [PROGRAMMERS][JAVA] 스티커 모으기(2) + 마법의 엘리베이터 + 시소 짝꿍 + 연속된 부분 수열의 합
    - 스티커 모으기(2) + 마법의 엘리베이터 + 시소 짝꿍 + 연속된 부분 수열의 합

--- programmers/han/Level 2-3/java/solution/Solution68.java
+++ programmers/han/Level 2-3/java/solution/Solution68.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/**
+ /**
  * 68. 삼각 달팽이
+ * 알고리즘 유형: 구현, 시뮬레이션
  */
 public class Solution68 {
+    /**
+     * @ Day1
+     * @param n
+     * @return
+     */
     public int[] solution(int n) {
         List<Integer> answer = new ArrayList<>();
         int[][] boxs = new int[n][n];
@@ -41,11 +47,8 @@ public class Solution68 {
         return answer.stream().mapToInt(i->i).toArray();
     }
 
-
-    
     public static void main(String[] args) {
         Solution68 s68 = new Solution68();
         s68.solution(4);
-        s68.solution2(4);
     }
 }
