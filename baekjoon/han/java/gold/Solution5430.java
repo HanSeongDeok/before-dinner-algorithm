8a340ee3f0b1bb96ca76f5b4e60c8dd000a13586
sdhan <hsd970730@naver.com>
2025-12-22 21:19:29 +0900

    [BAEKJOON] AC + 좌표 압축
    - AC + 좌표 압축

--- baekjoon/han/java/gold/Solution5430.java
+++ baekjoon/han/java/gold/Solution5430.java
@@ -10,44 +10,55 @@ import java.util.*;
  * 시간 초과 -> 오답 다시 풀기
  */
 public class Solution5430 {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int TC = Integer.parseInt(st.nextToken());
+        int TC = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
 
         for (int tc = 0; tc < TC; tc++) {
-            st = new StringTokenizer(br.readLine());
-            char[] COMMAND = st.nextToken().toCharArray();
+            boolean IS_REVERSE = false; 
+            boolean IS_ERROR = false;
 
-            st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken());
+            char[] COMMAND = br.readLine().toCharArray();
+            int N = Integer.parseInt(br.readLine());
 
-            st = new StringTokenizer(br.readLine());
+            String arrInput = br.readLine();
+            Deque<Integer> box = new ArrayDeque<>();
 
-            String[] ARR = st.nextToken().replaceAll("[\\[\\]]", "").split(",");
-            int[] box = N == 0 ? new int[]{} : Arrays.stream(ARR).mapToInt(Integer::parseInt).toArray();
-
-            for (char c : COMMAND) {
-                int[] copyBox = Arrays.copyOf(box, box.length);
-                if (c == 'R') {
-                    for (int i = 0; i < N; i++) {
-                        box[i] = copyBox[(N-1)-i];
-                    } 
-                } 
-                else if (c == 'D') {
-                    if (box.length == 0) break;
-                    int[] createBox = new int[copyBox.length-1];
-                    for (int i=0; i < copyBox.length-1; i++) {
-                        createBox[i] = copyBox[i+1];
-                    } 
-                    box = createBox;
+            if (N > 0) {
+                String[] ARR = arrInput.replaceAll("[\\[\\]]", "").split(",");
+                for (String s : ARR) {
+                    box.add(Integer.valueOf(s));
                 }
             }
-            String answer = box.length == 0 ? "error" : Arrays.toString(box); 
+
+            for (char c : COMMAND) {
+                if (c == 'R') {
+                    IS_REVERSE = !IS_REVERSE;
+                } else if (c == 'D') {
+                    if (box.isEmpty()) {
+                        IS_ERROR = true;
+                        break;
+                    }
+                    if (IS_REVERSE) box.pollLast();
+                    else box.pollFirst();
+                }
+            }
+
+            if (IS_REVERSE)  {
+                Deque<Integer> dq = new ArrayDeque<>();
+                int size = box.size();
+                for (int i=0; i<size; i++) {
+                    dq.add(box.pollLast());
+                }
+                box = dq;
+            } 
+
+            String answer = IS_ERROR ? "error" : box.toString().replaceAll(" ", "");
             sb.append(answer).append("\n");
         }
+
+        System.out.println("----------------------------");
         System.out.println(sb.toString());
     }
 }
