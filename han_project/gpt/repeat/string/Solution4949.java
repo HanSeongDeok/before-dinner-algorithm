a54c7dfcfbca88b38a72ddc1e08620e3459a70b4
sdhan <sdhan@suresofttech.com>
2026-01-16 14:51:59 +0900

    [BAEKJOON][REAPEAT] 자료구조 복습 - 균형 잡힌 세상

---
+++ han_project/gpt/repeat/string/Solution4949.java
@@ -0,0 +1,39 @@
+package repeat.string;
+import java.io.*;
+import java.util.*;
+
+public class Solution4949 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        while(true) {
+            String word = br.readLine();
+            if (word.equals(".")) break;
+
+            String answer = "yes";
+            Deque<Character> seqDq = new ArrayDeque<>();
+            
+            for (char w : word.toCharArray()) {
+                if (w == '(' || w == '[') seqDq.add(w);
+                else if (w == ')' || w == ']') {
+                    if (seqDq.isEmpty()) {
+                        answer = "no";
+                        break;
+                    }
+                    char compareW = seqDq.pollLast();
+                    boolean isMatch = true;
+                    if (w == ')' ) {
+                        if (compareW != '(') isMatch = false;
+                    } else if (w == ']' ) {
+                        if (compareW != '[') isMatch = false;
+                    }
+                    if (isMatch) continue;
+                    answer = "no";
+                    break;
+                }
+            }
+            answer = seqDq.isEmpty() ? "yes" : "no";
+            System.out.println(answer);
+        }
+    }
+}
